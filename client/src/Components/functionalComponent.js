2f2331b35e611a884666690fc55c5eb2be9632f2
Cullen Smith <cullensmi@gmail.com>
2020-09-25 09:32:46 -0400

    first commit

---
+++ client/src/Components/functionalComponent.js
@@ -0,0 +1,17 @@
+//Import Reat, hooks and any dependent functions from 'react' and other files
+import React, { useState } from 'react'
+
+//Defines and exports functional components so we can use it globally
+export default function functionalComponent() {
+
+    //Define our useState hook within our function body
+    const 
+
+
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
